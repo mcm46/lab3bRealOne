7b1cf4d82d66982cdbe604738e2f856858f6e577
Chris Muto <mcm46@duke.edu>
2012-11-30 20:16:59 -0500

    deleted treemap

--- src/virtualdisk/DVirtualDisk.java
+++ src/virtualdisk/DVirtualDisk.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 import common.Constants.DiskOperationType;
 import common.Constants;
