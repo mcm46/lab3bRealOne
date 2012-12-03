e27a5d772b4fe2d8b8eb6ea341a4031941c20f4d
Patrick <yanksfan2344@gmail.com>
2012-12-02 22:56:23 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Map;
 
 import common.Constants;
 import common.DFileID;
@@ -46,7 +47,7 @@ public class Main
 //		
 //		buff.waitClean();
 //		
-		//byte[] test = new byte[Constants.BLOCK_SIZE * 4];
+		byte[] test = new byte[Constants.BLOCK_SIZE * 4];
 //		
 //		for(int i = 0; i < test.length; i++)
 //		{
@@ -56,33 +57,35 @@ public class Main
 //			}
 //		}
 //		
-//		myFileSystem.read(new DFileID(2), test, 0, Constants.BLOCK_SIZE * 4);
+		myFileSystem.read(new DFileID(2), test, 0, Constants.BLOCK_SIZE * 4);
 		
-		Buffer test = new Buffer(2, Constants.BLOCK_SIZE);
-		
-		byte[] testBuffer = new byte[Constants.BLOCK_SIZE * 4];
-		
-		byte counter = 0;
-		for(int i = 0; i < Constants.BLOCK_SIZE * 4; i++)
-		{
-			testBuffer[i] = counter;
-			
-			counter++;
-			
-			if(counter > 255)
-			{
-				counter = 0;
-			}
-		}
-		
-		myFileSystem.write(new DFileID(2), testBuffer, 0, Constants.BLOCK_SIZE * 4);
-		
-		int testSize = myFileSystem.sizeDFile(new DFileID(2));
-		
-		byte[] testBufferRead = new byte[Constants.BLOCK_SIZE * 4];
-		
-		myFileSystem.read(new DFileID(2), testBufferRead, 0, Constants.BLOCK_SIZE * 4);
-		
-		myCache.sync();
+//		Buffer test = new Buffer(2, Constants.BLOCK_SIZE);
+//		
+//		byte[] testBuffer = new byte[Constants.BLOCK_SIZE * 4];
+////		
+//		byte counter = 0;
+//		for(int i = 0; i < Constants.BLOCK_SIZE * 4; i++)
+//		{
+//			testBuffer[i] = counter;
+//			
+//			counter++;
+//			
+//			if(counter > 255)
+//			{
+//				counter = 0;
+//			}
+//		}
+//		
+//		myFileSystem.write(new DFileID(2), testBuffer, 0, Constants.BLOCK_SIZE * 4);
+//		
+//		int testSize = myFileSystem.sizeDFile(new DFileID(2));
+//		
+//		Map<Integer, Boolean> p = myDisk.myBitmap;
+//		
+//		byte[] testBufferRead = new byte[Constants.BLOCK_SIZE * 4];
+//		
+//		myFileSystem.read(new DFileID(2), testBufferRead, 0, Constants.BLOCK_SIZE * 4);
+//		
+//		myCache.sync();
 	}
 }
